2afd203fa5909a7854fbfd244a7a5635b8a8edd4
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-14 08:32:29 -0200

    Handling events & displaying list

--- TODO_App/client/src/todo/todo.jsx
+++ TODO_App/client/src/todo/todo.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
   constructor(props) {
     super(props)
 
     this.state = { description: '', list: [] }
+    this.refresh()
   }
+
+  refresh(description = '') {
+    const search = description ? `&description__regex=/${description}/` : ''
+    axios.get(`${URL}?sort=-createdAt${search}`)
+    .then(res => this.setState({ ...this.state, description, list: res.data }))
+  }
+
   handleAdd() {
-    console.log(this)
+    console.log(this.state.description)
+    axios.post(URL, { description: this.state.description })
+    .then(res => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+    .then(res => this.refresh(this.state.description))
+  }
+
+  handleDone(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+    .then(res => this.refresh(this.state.description))
+  }
+
+  handleUndo(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+    .then(res => this.refresh(this.state.description))
+  }
+
+  handleSearch() {
+    this.refresh(this.state.description)
+  }
+
+  handleClear() {
+    this.refresh()
   }
 
   handleChange(e) {
@@ -26,8 +62,15 @@ export default class Todo extends Component {
           handleAdd={this.handleAdd.bind(this)}
           handleChange={this.handleChange.bind(this)}
           description={this.state.description}
+          handleSearch={this.handleSearch.bind(this)}
+          handleClear={this.handleClear.bind(this)}
         />
-        <TodoList />
+        <TodoList 
+          list={this.state.list} 
+          handleRemove={this.handleRemove.bind(this)}
+          handleDone={this.handleDone.bind(this)}
+          handleUndo={this.handleUndo.bind(this)}
+        />
       </div>
     )
   }
--- TODO_App/client/src/todo/todoList.jsx
+++ TODO_App/client/src/todo/todoList.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
-export default props => (
-  <div>
-    <h1>List</h1>
-  </div>
-)
+import IconButton from '../template/iconButton'
+
+export default props => {
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'done' : ''}>{todo.description}</td>
+        <td>
+          <IconButton style='success' icon="check" hide={todo.done}
+            onClick={() => props.handleDone(todo)}
+          />
+          <IconButton style="warning" icon="undo" hide={!todo.done}
+            onClick={() => props.handleUndo(todo)}
+          />
+          <IconButton style='danger' icon="trash-o" hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}
+          />
+        </td>
+      </tr>
+    ))
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th className="tableActions">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
